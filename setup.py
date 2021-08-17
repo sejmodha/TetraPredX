3d43a685b913151d591f01041414b738a5655a51
sejmodha <sejmodha@users.noreply.github.com>
2021-08-17 15:16:47 +0100

    Package dist created for PyPI submission

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup
+import setuptools
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
 setup(name='TetraPredX',
-      packages=['TetraPredX'],
+      packages=setuptools.find_packages(where="src", exclude=("tests",)),
       description='Prediction tool to identify the class of unknown sequences assembled from metagenomes',
       long_description=readme(),
       long_description_content_type='text/markdown',
-      version='1.0',
+      version='1.1',
       author='Sejal Modha',
       author_email='s.modha.1@research.gla.ac.uk',
       url='https://github.com/sejmodha/TetraPredX',
       download_url='https://github.com/sejmodha/TetraPredX/archive/refs/tags/1.0.tar.gz',
       install_requires=['biopython', 
                         'joblib', 
-                        'sklearn=0.24.1',
+                        'scikit-learn==0.24.1',
                         'pathos', 
                         'seaborn'],
       classifiers=[
